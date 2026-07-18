a66d7fc37434ea9bc3270e1d5a4f2f4fea480833
Devesh Gupta <deveshgupta1619@gmail>
2026-07-18 16:34:37 +0530

    updaitng butoon disablity

--- routes/Authroutes/UserAuthentication.js
+++ routes/Authroutes/UserAuthentication.js
@@ -98,7 +98,7 @@ router.post('/register',async(req,res)=>{
    try {
          const { FullName, useremail, username, password, mobile, roleName } = req.body;
       
-           console.log( FullName, useremail, username, password, mobile, roleName );
+         //   console.log( FullName, useremail, username, password, mobile, roleName );
            
            
          const existData = await userModel.findOne({ username: username })
@@ -177,17 +177,18 @@ router.post('/forgot_pass',async(req,res)=>{
       //   http://localhost:5173/reset-pass/${token}
 
 
-      console.log("EMAIL_USER:", process.env.EMAIL_USER);
-      console.log("JWT_SECRET:", process.env.JWT_SECRET ? "Present" : "Missing");
+      // console.log("EMAIL_USER:", process.env.EMAIL_USER);
+      // console.log("JWT_SECRET:", process.env.JWT_SECRET ? "Present" : "Missing");
 
 
 
 
       try {
     await transportter.verify();
-    console.log("SMTP connection successful");
+   //  console.log("SMTP connection successful");
 } catch (err) {
-    console.error("SMTP Verify Error:", err);
+   res.json(err.message)
+   //  console.error("SMTP Verify Error:", err);
 }
       
       await transportter.sendMail({
@@ -209,7 +210,7 @@ router.post('/forgot_pass',async(req,res)=>{
       res.status(200).json({message:`reset password mail sends successfully on ${useremail} `})
 
    } catch (error) {
-   console.error("Forgot Password Error:", error);
+   
 
    res.status(500).json({
       message: error.message,
